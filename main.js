74048d13edc54241147e540f97472bc7c8b009ea
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-09-13 19:27:00 +0800

    feat: implement htmlStatement

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-import { statement } from './statement/statement.js';
+import { statement, htmlStatement } from './statement/statement.js';
 import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 var invoices = require('./statement/invoices.json');
 var plays = require('./statement/plays.json');
 console.log(statement(invoices, plays));
+console.log(htmlStatement(invoices, plays));
