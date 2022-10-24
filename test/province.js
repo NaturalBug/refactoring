8dbeb14e3ef432f1324bcdbcc6f469d8cde47590
Cheng-Ying Li <chengying.li@bridgewell.com>
2022-10-24 19:12:43 +0800

    refactor: use beforeEach to initialize asia

--- test/province.js
+++ test/province.js
@@ -8,13 +8,16 @@ var describe = mocha.describe;
 var it = mocha.it;
 
 describe("province", function () {
+  let asia;
+  this.beforeEach(function () {
+    asia = new Province(sampleProvinceData());
+  });
+
   it("shortfall", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).equal(5);
   });
 
   it("profit", function () {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).equal(230);
   });
 });
