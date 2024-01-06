be515951b52b9c1829833c4244904046fcf57563
Cheng-Ying Li <chengying.li@bridgewell.com>
2024-01-06 11:48:04 +0800

    refactor: introduce Organization

--- organization/organization.js
+++ organization/organization.js
@@ -1,5 +1,18 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+const organization = new Organization({
+    name: 'Acme Gooseberries',
+    country: 'GB',
+});
 
 function getRawDataOfOrganization() {
+    return organization._data;
+}
+
+function getOrganization() {
     return organization;
+}
+
+class Organization {
+    constructor(data) {
+        this._data = data;
+    }
 }
